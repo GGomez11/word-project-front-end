f08ee17bf9dffae4cb067384aade459ec0ba5ce3
Greg <Greggomez11235@gmail.com>
2022-08-07 19:44:01 -0500

    Validation that passwords are the same.

--- src/components/Login/RegisterForm.js
+++ src/components/Login/RegisterForm.js
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import '../../css/GetStartedModal.css'
 import Button from '@material-ui/core/Button';
 import { useState } from 'react'
+import axios from 'axios'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,18 +20,44 @@ function RegisterForm() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [formError, setFormError] = useState({ isError: false, message: '' })
+
+    const passwordsMatch = () => {
+        if (password !== confirmPassword) {
+            setFormError(formError => ({ ...formError, isError: true, message: 'Password don\'t match' }))
+            return false
+        } else {
+            return true
+        }
+    }
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        if (passwordsMatch(password, confirmPassword)) {
+            axios.request({
+                method: 'post',
+                url: 'http://localhost:5000/login/register',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                data: {
+                    email: email,
+                    password: password
+                }
+            })
+            setFormError(formError => ({ ...formError, isError: false, message: '' }))
+        }
 
+        console.log(formError)
+        e.preventDefault()
     }
 
     return (
         <div className="form-container">
-            <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit} >
-                <TextField id="email" label="Email" fullWidth onChange={(e) => { setEmail(e.target.value) }} />
-                <TextField id="password" label="Password" type="password" fullWidth onChange={(e) => { setPassword(e.target.value) }} />
-                <TextField id="confirmPassword" label="Confirm Password" type="password" fullWidth onChange={(e) => { setConfirmPassword(e.target.value) }} />
+            <form className={classes.root} autoComplete="off" onSubmit={handleSubmit} >
+                <TextField id="email" label="Email" fullWidth onChange={(e) => { setEmail(e.target.value) }} required />
+                <TextField id="password" label="Password" type="password" error={formError.isError} fullWidth onChange={(e) => { setPassword(e.target.value) }} required helperText={formError.message} />
+                <TextField id="confirmPassword" label="Confirm Password" type="password" error={formError.isError} fullWidth onChange={(e) => { setConfirmPassword(e.target.value) }} required />
                 <Button type='submit' variant="outlined" color="primary">
                     Sign Up
                 </Button>
