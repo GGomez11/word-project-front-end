5e2e495e1322ab8ccd1eccdd1bc88948ef2b184d
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2022-09-01 21:23:24 -0500

    Create Validation For User and Password

---
+++ src/services/validateInput.js
@@ -0,0 +1,26 @@
+export function validateEmail(email){
+    if(email.length === 0) {
+        return false
+    }
+
+    let lastAtPos = email.lastIndexOf("@")
+    let lastDotPos = email.lastIndexOf(".")
+
+    if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          email.indexOf("@@") === -1 &&
+          lastDotPos > 2 &&
+          email.length - lastDotPos > 2
+        )
+      ) {
+        return false
+      } 
+
+    return true
+}
+
+export function validatePassword(password){
+    return true
+}
