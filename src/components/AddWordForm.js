f7ac79d6cbab0409a663bcee2039dbdbbb0a439a
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2022-09-04 19:42:18 -0500

    Remove Word API Request For Adding Word

--- src/components/AddWordForm.js
+++ src/components/AddWordForm.js
@@ -15,77 +15,29 @@ function AddWordForm(props){
         }
     }
 
-    /** 
-     * Function to parse the API response 
-     * Returns an array of objects
-     */
-    const parse = (response) => {
-        /**
-         * Format of the object being sent to database
-         * const example = {
-         *     word: response.word,
-         *     definitions: [{
-         *         definition: '',
-         *         partsOfSpeech: '',
-         *         synonym: [],    
-         *     }],
-         *     link: ''  
-         * }
-         */ 
-
-        // Iterate through every definition
-        // For each definition, get the part of speech and synonyms for that definition
-        let parsedSyn = response.results.map(result => {
-            /** Gets synonyms for the given definition, if a synonym for the definition exists  */
-            if(result.synonyms){
-                var synms = result.synonyms.map(synonym => {
-                    return synonym
-                })
-            }
-            /** Returns one object */
-            return {definition: result.definition, partsOfSpeech: result.partOfSpeech, synonym: synms}
-        })
-        /** Returns array of objects */
-        return parsedSyn
-    }  
-
-    /** 
-     * Handles submit event
-     * Make a get request to the RapidApi
-     * Post the word into my local database
-    */
     const handleSubmit = (e) => {
-        axios.request(options)
-            .then(res => {
-                console.log(res.data)
-                let parsedData = parse(res.data)
-                const token = localStorage.getItem('jwt')
-                const encodedString = Buffer.from(`Bearer ${token}`).toString('base64')
-                
-                axios.post('http://localhost:5000/words',{
-                    word: word,
-                    definitions: parsedData,
-                    link: 'https://translate.google.com/?hl=en&sl=auto&tl=es&text='+word+'&op=translate'
-                }, {headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': encodedString
-                }})
-                .then(res => {
-                    e.target.reset()
-                    props.getWords()
-                    console.log(res)  
-                })
-                .catch(err => {
-                    e.target.reset()
-                    console.log(err)
-                })
-            })
-            .catch(err => {
-                console.log(err)
-                alert(word + " not found in API database")
-                e.target.reset()
-            })
+        const token = localStorage.getItem('jwt')
+        const encodedString = Buffer.from(`Bearer ${token}`).toString('base64')
+
+        axios.post('http://localhost:5000/words',{
+            word: word
+        },{headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': encodedString
+        }})
+        .then(res => {
+            e.target.reset()
+            if (res.data.isAdded){
+                props.getWords()
+            }
+            console.log(res)  
+        })
+        .catch(err => {
+            e.target.reset()
+            console.log(err)
+        }) 
+
         e.preventDefault()
     }
 
