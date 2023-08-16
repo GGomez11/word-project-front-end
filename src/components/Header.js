0797384c9f900547dbc23fc2586330fe38659f04
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2023-08-16 12:16:38 -0500

    remove dark mode button from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,11 @@ function Header ({setIsLoggedIn, setDarkMode, darkMode}) {
     const removeToken = (e) => {
         localStorage.removeItem('jwt')
         setIsLoggedIn(false)
-        console.log('Remove Token')
     } 
 
     const toggleDarkMode = (e) => {
         const darkModeStorage = localStorage.getItem('darkmode') !== null ? !localStorage.getItem('darkmode') : 'false'
         setDarkMode(darkModeStorage)
-        console.log(darkModeStorage)
         localStorage.removeItem('darkmode')
         localStorage.setItem('darkmode', darkModeStorage)
     }
@@ -25,7 +23,7 @@ function Header ({setIsLoggedIn, setDarkMode, darkMode}) {
         <nav className="nav-style">
             <Link className="home-style" to="/"><HomeIcon sx={{ color: "#E6E6E6" }}/></Link>
             <Button className="logout-style" onClick={removeToken}><LogoutIcon sx={{ color: "#E6E6E6" }}/></Button>
-            <Button className="darkmode-style" onClick={toggleDarkMode}><DarkModeIcon sx={{ color: "#E6E6E6" }}/></Button>
+            {/* <Button className="darkmode-style" onClick={toggleDarkMode}><DarkModeIcon sx={{ color: "#E6E6E6" }}/></Button> */}
         </nav>
     )
 }
